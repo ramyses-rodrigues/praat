9f2d37b3e55d6b3ad0c24003b886dddbc41c0517
PaulBoersma <paul.boersma@uva.nl>
2025-10-05 09:09:36 +0200

    avoid reinterpret_cast

--- LPC/LPC_and_Formant.cpp
+++ LPC/LPC_and_Formant.cpp
@@ -4,7 +4,7 @@
  *
  * This code is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
+ * the Free Software Foundation; either version 3 of the License, or (at
  * your option) any later version.
  *
  * This code is distributed in the hope that it will be useful, but
@@ -57,7 +57,7 @@ void structLPCFrameIntoFormantFrame :: initBasicLPCFrameIntoFormantFrame (constL
 }
 
 void structLPCFrameIntoFormantFrame :: copyBasic (constSampledFrameIntoSampledFrame other2) {
-	constLPCFrameIntoFormantFrame other = reinterpret_cast<constLPCFrameIntoFormantFrame> (other2);
+	constLPCFrameIntoFormantFrame other = static_cast <constLPCFrameIntoFormantFrame> (other2);
 	LPCFrameIntoFormantFrame_Parent :: copyBasic (other);
 	our inputLPC = other -> inputLPC;
 	our outputFormant = other -> outputFormant;
